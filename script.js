16100e1a523105b7dac91f4149c4e17cd9571a3e
Bustamor <107319576+Bustamor@users.noreply.github.com>
2023-02-25 23:54:24 -0800

    plant-test

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+function makeboxdisappear(element){
+var target = document.querySelector(".acceptbox")
+target.remove() 
+}
+
+
+function changetoech(element){
+    // TARGET THE IMAGE
+    var image = document.querySelector("#bigcactus")
+    // CHANGE THE src to ech
+    image.src = "img/succulents-2.jpg"
+}
+
+
+
+function changetoaloe(element){
+    // TARGET THE IMAGE
+    var image = document.querySelector("#bigcactus")
+    // CHANGE the src to aloe
+    image.src = "img/succulents-1.jpg"
+}
